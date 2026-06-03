28a8c8744424d33c524d9748b7bb9c226b2b4c29
mstdlk14-beep <mstdlk14@gmail.com>
2026-06-03 12:15:07 +0300

    Update gb-fiyatlari.js

--- netlify/functions/gb-fiyatlari.js
+++ netlify/functions/gb-fiyatlari.js
@@ -1,10 +1,21 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      status: "ok",
-      source: "KOlayPazar",
-      message: "Canlı GB Function aktif"
-    })
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: "ok",
+        source: "Firebase gbFiyatlari",
+        site: "ByNoGame",
+        price: 1250
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: "error",
+        message: error.message
+      })
+    };
+  }
 };
